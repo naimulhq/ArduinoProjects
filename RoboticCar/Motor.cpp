bfa1c7617c89e180a10bfb25ff89ab6a8f8cd3fa
Naimul Hoque <naimul@ucsb.edu>
2021-11-21 16:36:48 -0800

    Naimul - Created Basic Motor Class for Elegoo Car

---
+++ RoboticCar/Motor.cpp
@@ -0,0 +1,7 @@
+/*
+
+Author: Naimul Hoque
+Updated: 11/21/2021
+Description: Motor.cpp for Motor Class based off of TB6612FNG Chip
+
+*/
---
+++ RoboticCar/Motor.h
@@ -0,0 +1,28 @@
+/*
+
+Author: Naimul Hoque
+Updated: 11/21/2021
+Description: Motor Class for Elegoo Smart-Car Shield V1.1 based off of TB6612FNG Chip
+
+*/
+#ifndef MOTOR_H
+#define MOTOR_H
+
+namespace elegoo{
+
+    class Motor{
+        private:
+            int _pwm_pin;
+            int _in_pin;
+        public:
+            Motor();
+            Motor(int,int);
+            virtual ~Motor();
+            void rotateClockwise();
+            void rotateCounterClockWise();
+            void stopMotor();
+    } 
+    
+}
+
+#endif
